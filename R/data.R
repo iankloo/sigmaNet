1996166d34f903a354ba32f1465b27a2488ed188
iankloo <iankloo@gmail.com>
2018-01-18 19:47:28 +0000

    added licensing and some docs

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Co-appearances of characters in Les Miserables
+#'
+#' A graph where the nodes are characters in Les Miserables and the 
+#' edges are times that the characters appeared together in the novel.
+#'
+#' @format An igraph object with 77 nodes and 254 edges
+#' \describe{
+#'   \item{id}{numeric id of nodes}
+#'   \item{label}{character label (names) of nodes}
+#'   \item{value}{numeric weight of the edges (number of co-appearances)}
+#' }
+#' @source D. E. Knuth, The Stanford GraphBase: A Platform for Combinatorial Computing, Addison-Wesley, Reading, MA (1993)
+"lesMis"
