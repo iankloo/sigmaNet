5ad9eb84d0998117b1ad9684015d299f929d49eb
Andrew Duncan <duncanaj2@gmail.com>
2018-04-30 21:17:56 +0100

    Problem was actually the difference in directed and undirected networks, should now be fixed.

--- R/sigmaAttrs.R
+++ R/sigmaAttrs.R
@@ -121,7 +121,8 @@ addNodeSize <- function(sigmaObj, minSize = 1, maxSize = 3, sizeMetric = 'degree
     sigmaObj$x$options$minNodeSize <- minSize
     sigmaObj$x$options$maxNodeSize <- maxSize
   } else{
-    tmp_graph <- igraph::graph_from_data_frame(sigmaObj$x$graph)
+    tmp_graph <- igraph::graph_from_data_frame(sigmaObj$x$graph$edges,
+                                               directed = FALSE)
     if(sizeMetric == 'degree'){
       nodes$size <- igraph::degree(tmp_graph)
     } else if(sizeMetric == 'closeness'){
